a12c4b72b368b0ca119a26d607051dc2b76f7f02
Fabien <fabienly@gmail.com>
2022-10-27 15:38:12 +0200

    block | Finish block and export config files.

--- web/modules/custom/adimeo_events/src/Plugin/Block/LinkedEvents.php
+++ web/modules/custom/adimeo_events/src/Plugin/Block/LinkedEvents.php
@@ -3,10 +3,13 @@
 
 namespace Drupal\adimeo_events\Plugin\Block;
 
-use Drupal;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Datetime\DrupalDateTime;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
 use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a custom Block to display linked event.
@@ -17,18 +20,78 @@ use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
  *   category = @Translation("Linked events"),
  * )
  */
-class LinkedEvents extends BlockBase {
+class LinkedEvents extends BlockBase implements ContainerFactoryPluginInterface{
+
+  /**
+   * The entity type manager.
+   *
+   * @var EntityTypeManagerInterface
+   */
+  protected $entity_type_manager;
+
+  /**
+   * Retrieves the currently active route match object.
+   *
+   * @var CurrentRouteMatch
+   */
+  protected $current_route;
+
+  /**
+   * Constructs a new block.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param CurrentRouteMatch $current_route
+   *   The current route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, CurrentRouteMatch $current_route) {
+    $this->entity_type_manager = $entity_type_manager;
+    $this->current_route = $current_route;
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+
+  /**
+   * Creates an instance of the plugin.
+   *    *
+   * @param ContainerInterface $container
+   *   The container to pull out services used in the plugin.
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   *
+   * @return static
+   *   Returns an instance of this plugin.
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): LinkedEvents {
+
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('current_route_match')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
-  public function build(): array
-  {
+  public function build(): array {
 
     $similarEvents = [];
 
     // Get event type from current route.
-    $node = Drupal::routeMatch()->getParameter('node');
+    $node = $this->current_route->getParameter('node');
     $eventType = $node->get('field_event_type')->target_id;
 
    // Get Similar Event not ended related to event type.
@@ -41,19 +104,27 @@ class LinkedEvents extends BlockBase {
     }
 
     // Load and build them all together with appropriated view mode.
-    $node_storage = \Drupal::entityTypeManager()->getStorage('node')->loadMultiple($similarEvents);
-    return \Drupal::entityTypeManager()->getViewBuilder('node')->viewMultiple($node_storage, 'linked_events');
+    $node_storage = $this->entity_type_manager->getStorage('node')->loadMultiple($similarEvents);
+    return $this->entity_type_manager->getViewBuilder('node')->viewMultiple($node_storage, 'linked_events');
   }
 
-  private function getSimilarEvents(int $eventType)
-  {
+  /**
+   *  Query for node entity
+   *
+   * @param int $eventType
+   *  Tid of the main event.
+   * @return array|int
+   *  Array of nid base on the query.
+   *
+   */
+  private function getSimilarEvents(int $eventType) {
 
     // Get current date and format it.
     $currentDate = new DrupalDateTime('now');
     $formatted = $currentDate->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT);
 
     // Query similar Events based on type and date
-    $query = Drupal::entityQuery('node');
+    $query = $this->entity_type_manager->getStorage('node')->getQuery();
     $query->condition('type', 'event');
     $query->condition('field_event_type', $eventType);
     $query->condition('field_date_end', $formatted, '>');
@@ -68,7 +139,7 @@ class LinkedEvents extends BlockBase {
       }
 
     // If not enough events found complete with others type.
-    $query = Drupal::entityQuery('node');
+    $query = $this->entity_type_manager->getStorage('node')->getQuery();
     $query->condition('type', 'event');
     $query->condition('field_event_type', [$eventType], 'NOT IN');
     $query->condition('field_date_end', $formatted, '>');
